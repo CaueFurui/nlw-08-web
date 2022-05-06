3bde4fdbca189d348004df5ec9a397af820707c6
cauefurui <cauefurui@gmail.com>
2022-05-06 15:34:37 -0300

    feat: finalizando o front

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import Bug from '../../assets/Bug.svg'
+import Idea from '../../assets/Idea.svg'
+import Thought from '../../assets/Thought.svg'
+import FeedbackTypeStep from "./Steps/FeedbackTypeStep";
+import FeedbackContentStep from "./Steps/FeedbackContentStep";
+import FeedbackSuccessStep from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: Bug,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: Idea,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: Thought,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  },
+}
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>()
+  const [feedbackSent, setFeedbackSent] = useState(false);
+  
+  function handleRestartFeedback() {
+    setFeedbackSent(false)
+    setFeedbackType(null)
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+        {feedbackSent ? (
+          <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+        ) : (
+          <>
+            {!feedbackType ? (
+              <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+              <FeedbackContentStep
+                onFeedbackRestartRequested={handleRestartFeedback}
+                feedbackType={feedbackType}
+                onFeedbackSent={() => setFeedbackSent(true)}
+              />
+            )}
+          </>
+        )}
+      
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela <a className="underline underline-offset-2" href='https://rocketseat.com.br'>Rocketseat</a>
+      </footer>
+    </div>
+  )
+}
